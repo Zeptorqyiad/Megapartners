cdcca24aa78d2a9afaf9e791ca68d642e902ccd6
Max Unanian <Zeptorqyiad99@gmail.com>
2024-05-22 22:01:16 +0500

    Finished with project

--- js/script.js
+++ js/script.js
@@ -9,3 +9,21 @@ languageButton.addEventListener('click', function () {
       languageText.textContent = 'EN'
    }
 })
+
+const headerMenu = document.querySelector('.burger-wrapper')
+const burgerMenu = document.querySelector('.menu__burger')
+const burgerPanel = document.querySelector('.burger')
+const burgerLine1 = document.querySelector('.rw-1')
+const burgerLine2 = document.querySelector('.rw-2')
+const burgerLine3 = document.querySelector('.rw-3')
+
+headerMenu.addEventListener('click', () => {
+   if (headerMenu) {
+      burgerMenu.classList.toggle('active')
+      burgerPanel.classList.toggle('active')
+      burgerLine1.classList.toggle('active')
+      burgerLine2.classList.toggle('active')
+      burgerLine3.classList.toggle('active')
+      document.body.classList.toggle('_lock')
+   }
+})
